a149c57f92a3abf577cee1954cb589fd93f0a439
GamerAmesia <p.deyzaguirre@gmail.com>
2024-07-04 16:10:09 +0200

    initial commit

--- vue-project/src/components/CarouselTest.vue
+++ vue-project/src/components/CarouselTest.vue
@@ -1,102 +1,92 @@
 <template>
-    <Carousel :itemsToShow="3.95" :wrapAround="true" :transition="500">
-        <Slide v-for="lieu in lieux" :key="lieu.id">
-        <div class="card text-center bg-[cyan]">
-          <h2 class="card-title">{{ lieu.titre }}</h2>
-          <img :src="lieu.image" class="mx-auto" />
-          <div class="card-content">
-            <p>{{ lieu.description }}</p>
-            <a :href="lieu.link" class="button">En Savoir plus ...</a>
-          </div>
+  <Carousel :itemsToShow="3.95" :wrapAround="true" :transition="500" class="my-10">
+    <Slide v-for="lieu in lieux" :key="lieu.id">
+      <div class="card text-center bg-[cyan]">
+        <h2 class="card-title">{{ lieu.titre }}</h2>
+        <img :src="lieu.image" class="mx-auto" />
+        <div class="card-content">
+          <p>{{ lieu.description }}</p>
+          <a :href="lieu.link" class="button">En Savoir plus ...</a>
         </div>
-      </Slide>
-  
-      ...
-    </Carousel>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue'
-  import { Carousel, Pagination, Slide } from 'vue3-carousel'
-  
-  import 'vue3-carousel/dist/carousel.css'
-  
-  export default defineComponent({
-    name: 'Autoplay',
-    components: {
-      Carousel,
-      Slide,
-      Pagination,
-    },
-    data() {
+      </div>
+    </Slide>
+
+    ...
+  </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Pagination, Slide } from 'vue3-carousel'
+import axios from 'axios'
+import 'vue3-carousel/dist/carousel.css'
+
+export default defineComponent({
+  name: 'Autoplay',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+  },
+  data() {
     return {
-      lieux: [
-        {
-          id: 1,
-          titre: 'Lieux 1',
-          image: '/images/eglise.jpg',
-          description: 'Ceci est une courte description pour le lieux 1, on verra ce que je marque ici.',
-          link: '#'
-        },
-        {
-          id: 2,
-          titre: 'Lieux 2',
-          image: '/images/chateau.jpg',
-          description: 'Ceci est une courte description pour le lieux 2, on verra ce que je marque ici.',
-          link: '#'
-        },
-        {
-          id: 3,
-          titre: 'Lieux 3',
-          image: '/images/musee.jpg',
-          description: 'Ceci est une courte description pour le lieux 3, on verra ce que je marque ici.',
-          link: '#'
-        }
-        // Ajoutez autant d'objets de lieux que nécessaire
-      ]
-    }
-  }
-  })
-  </script>
-  
-  <style scoped>
-  .carousel__slide {
-    padding: 5px;
-  }
-  
-  .carousel__viewport {
-    perspective: 2000px;
-  }
-  
-  .carousel__track {
-    transform-style: preserve-3d;
-  }
-  
-  .carousel__slide--sliding {
-    transition: 0.5s;
-  }
-  
-  .carousel__slide {
-    opacity: 0.9;
-    transform: rotateY(-20deg) scale(0.9);
-  }
-  
-  .carousel__slide--active ~ .carousel__slide {
-    transform: rotateY(20deg) scale(0.9);
-  }
-  
-  .carousel__slide--prev {
-    opacity: 1;
-    transform: rotateY(-10deg) scale(0.95);
-  }
-  
-  .carousel__slide--next {
-    opacity: 1;
-    transform: rotateY(10deg) scale(0.95);
-  }
-  
-  .carousel__slide--active {
-    opacity: 1;
-    transform: rotateY(0) scale(1.1);
-  }
-  </style>
+      lieux: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('/DataLieuxCult.json');
+        this.lieux = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+})
+</script>
+
+<style scoped>
+.carousel__slide {
+  padding-left: 25px;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(1);
+}
+
+.carousel__slide--active~.carousel__slide {
+  transform: rotateY(20deg) scale(1);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(1);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(1);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1);
+}
+</style>
---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="border border-transparent" id="bgTOP">
+        <div class="relative mx-auto my-2 px-4 sm:px-6 lg:px-8">
+            <div class="flex flex-col md:flex-row h-auto md:h-60 items-center md:items-start justify-between">
+                <!-- Empty div for centering purposes -->
+                <div class="flex-1"></div>
+                <!-- BLOC LOGO -->
+                <div class="flex flex-col justify-center items-center w-full md:w-auto">
+                    <div class="flex flex-row justify-center md:-mt-10">
+                        <img src="/images/Logo_Transparent.png" alt="Logo Bon Tourisme" class="w-32 md:w-auto" />
+                    </div>
+                    <div class="flex flex-row justify-center mt-2 md:-mt-10">
+                        <h1 class="oneLine text-[#763538] italic font-extralight opacity-75 text-center">
+                            Ne plus être déçu est notre façon de profiter
+                        </h1>
+                    </div>
+                </div>
+                <!-- BLOC CONNECTION -->
+                <div
+                    class="absolute top-2 right-4 md:relative md:top-0 md:right-0 md:flex-1 md:flex md:justify-end md:self-start flex items-center gap-4 mt-4 md:mt-0">
+                    <div class="flex gap-4">
+                        <a class="rounded-md bg-[#763538] px-5 py-2.5 text-sm font-medium invisible md:visible md:hover:bg-white md:hover:text-[#763538]"
+                            href="#">
+                            S'enregistrer
+                        </a>
+                        <div class="flex gap-4 z-10">
+                            <a class="rounded-md bg-[#763538] text-white hover:bg-white hover:text-[#763538] px-5 py-2.5 text-sm font-medium"
+                                href="#">
+                                Connection
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+#bgTOP{
+    background-image: url(/images/Baie_Rouge.jpg);
+    background-position: 50% 38%;
+    background-repeat: no-repeat;
+}
+@media (max-width: 768px) {
+    #bgTOP {
+        background-image: none;
+    }
+}
+</style>
