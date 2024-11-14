1605e4d9acadabd35b013b8a4afae85e4af59c59
Pierre de Yzaguirre <p.deyzaguirre@gmail.com>
2024-11-14 10:19:35 +0100

    initial commit

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup>
-import CarouselCult from "../components/CarouselCult.vue"
-import CarouselResto from "../components/CarouselResto.vue"
+import CarouselCult from "../components/CarouselCult.vue";
+import CarouselResto from "../components/CarouselResto.vue";
 </script>
 
 <template>
-
   <main>
     <CarouselCult />
     <CarouselResto />
